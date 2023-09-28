8a999ceac8bb4170ea2cccf42847fe8c437900b7
heasp04 <heasp2004@gmail.com>
2023-09-28 14:26:58 +0200

    Fixed PlayerInput and PlayerMovement
    You should be able to move now, and if you can't well... Too bad

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    [Header("Movement")] 
+    [HideInInspector] public Vector2 moveDirection;
+
+    [Header("Jump")] 
+    [HideInInspector] public bool canJump;
+    [HideInInspector] public bool jumpPressed, jumpReleased, jumpHeld;
+
+    [Header("Interact")] 
+    [HideInInspector] public bool canInteract;
+    [HideInInspector] public bool interactPressed, interactReleased, interactHeld;
+
+    [SerializeField] private bool usingGamepad, usingDpad;
+
+    private Keyboard _keyboard;
+    private Gamepad _gamepad;
+
+    private void Start()
+    {
+        _keyboard = Keyboard.current;
+        _gamepad = Gamepad.current;
+    }
+
+    private void Update()
+    {
+        if (usingGamepad && _gamepad != null)
+        {
+            UpdateGamepadInput();
+        }
+        else
+        {
+            UpdateKeyboardInput();
+        }
+    }
+
+    private void UpdateKeyboardInput()
+    {
+        moveDirection.x = (_keyboard.dKey.isPressed ? 1 : 0) + (_keyboard.aKey.isPressed ? -1 : 0);
+        moveDirection.y = (_keyboard.wKey.isPressed ? 1 : 0) + (_keyboard.sKey.isPressed ? -1 : 0);
+
+        jumpPressed = _keyboard.spaceKey.wasPressedThisFrame;
+        jumpReleased = _keyboard.spaceKey.wasReleasedThisFrame;
+        jumpHeld = _keyboard.spaceKey.isPressed;
+
+        interactPressed = _keyboard.fKey.wasPressedThisFrame;
+        interactReleased = _keyboard.fKey.wasReleasedThisFrame;
+        interactHeld = _keyboard.fKey.isPressed;
+    }
+
+    private void UpdateGamepadInput()
+    {
+        if (usingDpad)
+        {
+            moveDirection.x = (_gamepad.dpad.right.isPressed ? 1 : 0) + (_gamepad.dpad.left.isPressed ? -1 : 0);
+            moveDirection.y = (_gamepad.dpad.up.isPressed ? 1 : 0) + (_gamepad.dpad.down.isPressed ? -1 : 0); 
+        }
+        else
+        {
+            moveDirection = _gamepad.leftStick.ReadValue();
+        }
+    
+        jumpPressed = _gamepad.buttonSouth.wasPressedThisFrame;
+        jumpReleased = _gamepad.buttonSouth.wasReleasedThisFrame;
+        jumpHeld = _gamepad.buttonSouth.isPressed;
+        
+        interactPressed = _gamepad.buttonEast.wasPressedThisFrame;
+        interactReleased = _gamepad.buttonEast.wasReleasedThisFrame;
+        interactHeld = _gamepad.buttonEast.isPressed;
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")] 
+    public float moveSpeed = 4f;
+    public float jumpSpeed = 10f;
+    private Vector2 _desiredVelocity;
+
+    [Header("CoyoteTime")] 
+    public float coyoteTime = 0.2f;
+    public float coyoteTimeCounter;
+
+    [Header("JumpBuffer")] 
+    public float jumpBufferTime = 0.2f;
+    public float jumpBufferCounter;
+    
+    [Header("Acceleration")]
+    public float accelerationTime = 0.02f;
+    public float groundFriction = 0.03f;
+    public float airFriction = 0.005f;
+    
+    [Header("isGrounded")]
+    public LayerMask whatIsGround;
+    
+    [Header("Audio")]
+    
+    [Header("Components")]
+    private Rigidbody2D _rigidbody2D;
+    private InputManager _input;
+    private PlayerAnimator _animation;
+    private AudioSource _audioSource;
+    
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _input = GetComponent<InputManager>();
+        _animation = GetComponent<PlayerAnimation>();
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _desiredVelocity = _rigidbody2D.velocity;
+
+        _animation.UpdateAnimation(_desiredVelocity, IsPlayerGrounded(), _input.moveDirection);
+
+        if (IsPlayerGrounded())
+        { coyoteTimeCounter = coyoteTime; }
+        else
+        { coyoteTimeCounter -= 1 * Time.deltaTime; }
+
+        if (_input.jumpPressed)
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= 1 * Time.deltaTime;
+        }
+
+        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0)
+        {
+            _desiredVelocity.y = jumpSpeed;
+            jumpBufferCounter = 0f;
+        }
+
+        if (_input.jumpReleased && _rigidbody2D.velocity.y > 0f)
+        {
+            _desiredVelocity.y *= 0.5f;
+            coyoteTimeCounter = 0f;
+        }
+        _rigidbody2D.velocity = _desiredVelocity;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_input.moveDirection.x != 0)
+        {
+            _desiredVelocity.x = Mathf.Lerp(_desiredVelocity.x,
+                moveSpeed * _input.moveDirection.x,
+                accelerationTime);
+        }
+        else
+        {
+            _desiredVelocity.x = Mathf.Lerp(_desiredVelocity.x, 0f, 
+                IsPlayerGrounded() ? groundFriction : airFriction);
+        }
+
+        _rigidbody2D.velocity = _desiredVelocity;
+    }
+
+    private bool IsPlayerGrounded()
+    {
+        return Physics2D.Raycast(transform.position,
+            Vector2.down,
+            1.6f,
+            whatIsGround);
+    }
+}
